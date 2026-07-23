803095b7657734464f144b9bdb1e12cb8c7d91f2
karthik16-ux <karthikpanugothu041@gmail.com>
2026-07-24 00:18:14 +0530

    Initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="https://github.com/karthik16-ux">Karthik</a>
                 </h4>
             </footer>
         </div>
